5a1053b353d91df32d7d7525c04c376ff4bd55e9
Michael Bridak <michael.bridak@gmail.com>
2023-02-10 08:37:37 -0800

    import CAT control lib abnd some stuff.

--- not1mm/__main__.py
+++ not1mm/__main__.py
@@ -8,7 +8,6 @@ import logging
 import os
 import pkgutil
 import re
-import socket
 import sqlite3
 import sys
 from datetime import datetime
@@ -24,13 +23,17 @@ from PyQt5.QtCore import QDir, Qt  # pylint: disable=no-name-in-module
 from PyQt5.QtGui import QFontDatabase  # pylint: disable=no-name-in-module
 
 try:
+    from not1mm.lib.cat_interface import CAT
     from not1mm.lib.cwinterface import CW
+    from not1mm.lib.ham_utility import *
     from not1mm.lib.lookup import QRZlookup
     from not1mm.lib.version import __version__
 
     # from not1mm.lib.settings import Settings
 except ModuleNotFoundError:
+    from lib.cat_interface import CAT
     from lib.cwinterface import CW
+    from lib.ham_utility import *
     from lib.lookup import QRZlookup
     from lib.version import __version__
 
@@ -38,7 +41,7 @@ except ModuleNotFoundError:
 
 
 loader = pkgutil.get_loader("not1mm")
-assert isinstance(loader, SourceFileLoader)
+assert isinstance(loader, SourceFileLoader)  # This makes the linter happy.
 WORKING_PATH = os.path.dirname(loader.get_filename())
 
 
@@ -46,6 +49,8 @@ class MainWindow(QtWidgets.QMainWindow):
     """
     The main window
     """
+
+    current_op = ""
 
     def __init__(self, *args, **kwargs):
         logging.info("MainWindow: __init__")
@@ -61,6 +66,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.reddot = QtGui.QPixmap(icon_path + "reddot.png")
         self.leftdot.setPixmap(self.greendot)
         self.rightdot.setPixmap(self.reddot)
+        self.rig_control = CAT("rigctld", "localhost", 4532)
 
     def callsign_changed(self):
         text = self.callsign.text()
@@ -96,6 +102,72 @@ class MainWindow(QtWidgets.QMainWindow):
     def get_opon(self):
         ...
 
+    def poll_radio(self):
+        if self.rig_control.online:
+            vfo = self.rig_control.get_vfo()
+            mode = self.rig_control.get_mode()
+            print(f"VFO: {vfo}  MODE: {mode}")
+
+    def read_cw_macros(self) -> None:
+        """
+        Reads in the CW macros, firsts it checks to see if the file exists. If it does not,
+        and this has been packaged with pyinstaller it will copy the default file from the
+        temp directory this is running from... In theory.
+        """
+
+        if not Path("./cwmacros.txt").exists():
+            # logger.debug("read_cw_macros: copying default macro file.")
+            data_path = self.working_path + "/data/cwmacros.txt"
+            copyfile(data_path, "./cwmacros.txt")
+        with open("./cwmacros.txt", "r", encoding="utf-8") as file_descriptor:
+            for line in file_descriptor:
+                try:
+                    mode, fkey, buttonname, cwtext = line.split("|")
+                    if mode.strip().upper() == "R" and self.run_state:
+                        self.fkeys[fkey.strip()] = (buttonname.strip(), cwtext.strip())
+                    if mode.strip().upper() != "R" and not self.run_state:
+                        self.fkeys[fkey.strip()] = (buttonname.strip(), cwtext.strip())
+                except ValueError as err:
+                    ...
+                    # logger.info("read_cw_macros: %s", err)
+        keys = self.fkeys.keys()
+        if "F1" in keys:
+            self.F1.setText(f"F1: {self.fkeys['F1'][0]}")
+            self.F1.setToolTip(self.fkeys["F1"][1])
+        if "F2" in keys:
+            self.F2.setText(f"F2: {self.fkeys['F2'][0]}")
+            self.F2.setToolTip(self.fkeys["F2"][1])
+        if "F3" in keys:
+            self.F3.setText(f"F3: {self.fkeys['F3'][0]}")
+            self.F3.setToolTip(self.fkeys["F3"][1])
+        if "F4" in keys:
+            self.F4.setText(f"F4: {self.fkeys['F4'][0]}")
+            self.F4.setToolTip(self.fkeys["F4"][1])
+        if "F5" in keys:
+            self.F5.setText(f"F5: {self.fkeys['F5'][0]}")
+            self.F5.setToolTip(self.fkeys["F5"][1])
+        if "F6" in keys:
+            self.F6.setText(f"F6: {self.fkeys['F6'][0]}")
+            self.F6.setToolTip(self.fkeys["F6"][1])
+        if "F7" in keys:
+            self.F7.setText(f"F7: {self.fkeys['F7'][0]}")
+            self.F7.setToolTip(self.fkeys["F7"][1])
+        if "F8" in keys:
+            self.F8.setText(f"F8: {self.fkeys['F8'][0]}")
+            self.F8.setToolTip(self.fkeys["F8"][1])
+        if "F9" in keys:
+            self.F9.setText(f"F9: {self.fkeys['F9'][0]}")
+            self.F9.setToolTip(self.fkeys["F9"][1])
+        if "F10" in keys:
+            self.F10.setText(f"F10: {self.fkeys['F10'][0]}")
+            self.F10.setToolTip(self.fkeys["F10"][1])
+        if "F11" in keys:
+            self.F11.setText(f"F11: {self.fkeys['F11'][0]}")
+            self.F11.setToolTip(self.fkeys["F11"][1])
+        if "F12" in keys:
+            self.F12.setText(f"F12: {self.fkeys['F12'][0]}")
+            self.F12.setToolTip(self.fkeys["F12"][1])
+
 
 def load_fonts_from_dir(directory: str) -> set:
     """
@@ -130,6 +202,7 @@ def run():
     """
 
     install_icons()
+    timer.start(1000)
 
     sys.exit(app.exec())
 
@@ -162,6 +235,8 @@ window = MainWindow()
 window.setGeometry(-1, -1, 600, 200)
 window.setWindowTitle(f"Not1MM v{__version__}")
 window.show()
+timer = QtCore.QTimer()
+timer.timeout.connect(window.poll_radio)
 
 
 if __name__ == "__main__":
