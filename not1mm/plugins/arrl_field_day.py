85b58945fe3ea62d2d45edad4f3ac9a110510597
Michael Bridak <michael.bridak@gmail.com>
2023-02-22 11:16:08 -0800

    add mode and dupe type

--- not1mm/plugins/arrl_field_day.py
+++ not1mm/plugins/arrl_field_day.py
@@ -1,6 +1,10 @@
 from PyQt5 import QtWidgets
 
 name = "ARRL Field Day"
+mode = "BOTH"  # CW SSB BOTH RTTY
+
+# 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
+dupe_type = 3
 
 
 def interface(self):
