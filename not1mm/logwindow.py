e8bead38a7b512f8991d3ce93af90f560571577c
Michael Bridak <michael.bridak@gmail.com>
2023-03-15 08:05:24 -0700

    Watched for db changes and updates the log display

--- not1mm/logwindow.py
+++ not1mm/logwindow.py
@@ -13,9 +13,10 @@ import sys
 from pathlib import Path
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtCore import QDir
-
+from PyQt5.QtCore import QDir, QItemSelectionModel
 from PyQt5.QtGui import QFontDatabase
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 from not1mm.lib.database import DataBase
 
@@ -35,6 +36,33 @@ else:
 CONFIG_PATH += "/not1mm"
 
 
+class Watcher:
+    """Keep an eye out"""
+
+    def __init__(self, directory=".", handler=FileSystemEventHandler()):
+        self.observer = Observer()
+        self.handler = handler
+        self.directory = directory
+
+    def start(self):
+        """Starts the file watcher"""
+        self.observer.schedule(self.handler, self.directory, recursive=True)
+        self.observer.start()
+
+    def stop(self):
+        """Stops the file watcher"""
+        self.observer.stop()
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+    """Make them accoutable"""
+
+    def on_modified(self, event):
+        if not event.is_directory:
+            window.get_log()
+
+
 class MainWindow(QtWidgets.QMainWindow):
     """
     The main window
@@ -48,29 +76,102 @@ class MainWindow(QtWidgets.QMainWindow):
         self.contact = self.database.empty_contact
         data_path = WORKING_PATH + "/data/logwindow.ui"
         uic.loadUi(data_path, self)
-        self.generalLog.setColumnCount(3)
-        item = QtWidgets.QTableWidgetItem()
-        self.generalLog.setHorizontalHeaderItem(0, item)
-        self.generalLog.setHorizontalHeaderItem(1, item)
-        self.generalLog.setHorizontalHeaderItem(2, item)
+        self.generalLog.setColumnCount(11)
+        # item = QtWidgets.QTableWidgetItem("")
+        self.generalLog.setHorizontalHeaderItem(0, QtWidgets.QTableWidgetItem("MM-DD"))
+        self.generalLog.setHorizontalHeaderItem(1, QtWidgets.QTableWidgetItem("HH:MM"))
+        self.generalLog.setHorizontalHeaderItem(2, QtWidgets.QTableWidgetItem("Call"))
+        self.generalLog.setHorizontalHeaderItem(3, QtWidgets.QTableWidgetItem("Freq"))
+        self.generalLog.setHorizontalHeaderItem(4, QtWidgets.QTableWidgetItem("Snt"))
+        self.generalLog.setHorizontalHeaderItem(5, QtWidgets.QTableWidgetItem("Rcv"))
+        self.generalLog.setHorizontalHeaderItem(6, QtWidgets.QTableWidgetItem("M1"))
+        self.generalLog.setHorizontalHeaderItem(7, QtWidgets.QTableWidgetItem("ZN"))
+        self.generalLog.setHorizontalHeaderItem(8, QtWidgets.QTableWidgetItem("M2"))
+        self.generalLog.setHorizontalHeaderItem(9, QtWidgets.QTableWidgetItem("PFX"))
+        self.generalLog.setHorizontalHeaderItem(10, QtWidgets.QTableWidgetItem("PTS"))
+        PATH = (
+            sys.argv[1]
+            if len(sys.argv) > 1
+            else "/home/mbridak/.local/share/not1mm/ham.db"
+        )
+        w = Watcher(PATH, Handler())
+        w.start()
+        self.get_log()
 
     def get_log(self):
         """Get Log, Show it."""
         current_log = self.database.fetch_all_contacts_asc()
+        self.generalLog.setRowCount(0)
         for log_item in current_log:
-            print(f"Log Item {log_item}")
-            numRows = self.generalLog.rowCount()
-            print(f"num rows {numRows}")
-            self.generalLog.insertRow(numRows)
+            number_of_rows = self.generalLog.rowCount()
+            self.generalLog.insertRow(number_of_rows)
+            time_stamp = log_item.get("TS", "MM-DD HH:MM")
+            month_day, hour_min = time_stamp.split(" ")
+            _, month, day = month_day.split("-")
+            hour, minute, _ = hour_min.split(":")
+            month_day = f"{month}-{day}"
+            hour_min = f"{hour}:{minute}"
+
+            first_item = QtWidgets.QTableWidgetItem(month_day)
+            self.generalLog.setItem(number_of_rows, 0, first_item)
+            self.generalLog.setCurrentItem(first_item, QItemSelectionModel.NoUpdate)
             self.generalLog.setItem(
-                numRows, 0, QtWidgets.QTableWidgetItem(log_item.get("TS", ""))
+                number_of_rows, 1, QtWidgets.QTableWidgetItem(hour_min)
+            )
+
+            self.generalLog.setItem(
+                number_of_rows,
+                2,
+                QtWidgets.QTableWidgetItem(str(log_item.get("Call", ""))),
             )
             self.generalLog.setItem(
-                numRows, 1, QtWidgets.QTableWidgetItem(log_item.get("Call", ""))
+                number_of_rows,
+                3,
+                QtWidgets.QTableWidgetItem(str(log_item.get("Freq", ""))),
             )
             self.generalLog.setItem(
-                numRows, 2, QtWidgets.QTableWidgetItem(str(log_item.get("Freq", "")))
+                number_of_rows,
+                4,
+                QtWidgets.QTableWidgetItem(str(log_item.get("SNT", ""))),
             )
+            self.generalLog.setItem(
+                number_of_rows,
+                5,
+                QtWidgets.QTableWidgetItem(str(log_item.get("RCV", ""))),
+            )
+            self.generalLog.setItem(
+                number_of_rows,
+                6,
+                QtWidgets.QTableWidgetItem(str(log_item.get("IsMultiplier1", ""))),
+            )
+            self.generalLog.setItem(
+                number_of_rows,
+                7,
+                QtWidgets.QTableWidgetItem(str(log_item.get("ZN", ""))),
+            )
+            self.generalLog.setItem(
+                number_of_rows,
+                8,
+                QtWidgets.QTableWidgetItem(str(log_item.get("IsMultiplier2", ""))),
+            )
+            self.generalLog.setItem(
+                number_of_rows,
+                9,
+                QtWidgets.QTableWidgetItem(str(log_item.get("WPXPrefix", ""))),
+            )
+            self.generalLog.setItem(
+                number_of_rows,
+                10,
+                QtWidgets.QTableWidgetItem(str(log_item.get("Points", ""))),
+            )
+        # print(
+        #     f"row count {self.generalLog.rowCount()} item {self.generalLog.item(self.generalLog.rowCount() - 1, 0)}"
+        # )
+        # self.generalLog.scrollToItem(
+        #     self.generalLog.item(self.generalLog.rowCount() - 1, 0),
+        #     QtWidgets.QAbstractItemView.EnsureVisible,
+        # )
+        # self.generalLog.selectRow(self.generalLog.rowCount() - 1)
 
 
 def load_fonts_from_dir(directory: str) -> set:
@@ -84,16 +185,15 @@ def load_fonts_from_dir(directory: str) -> set:
     return font_families
 
 
+app = QtWidgets.QApplication(sys.argv)
+font_path = WORKING_PATH + "/data"
+_families = load_fonts_from_dir(os.fspath(font_path))
+window = MainWindow()
+window.show()
+
+
 def main():
     """main entry"""
-    app = QtWidgets.QApplication(sys.argv)
-    font_path = WORKING_PATH + "/data"
-    _families = load_fonts_from_dir(os.fspath(font_path))
-    window = MainWindow()
-    window.show()
-    timer = QtCore.QTimer()
-    timer.timeout.connect(window.get_log)
-    timer.start(1000)
     sys.exit(app.exec())
 
 
