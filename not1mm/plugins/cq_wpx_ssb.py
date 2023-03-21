cbe85b241c44ab4d9d8708b7130f627af4936e87
Michael Bridak <michael.bridak@gmail.com>
2023-03-21 13:46:13 -0700

    add stub

---
+++ not1mm/plugins/cq_wpx_ssb.py
@@ -0,0 +1,74 @@
+"""CQ WPX SSB plugin"""
+from PyQt5 import QtWidgets
+
+name = "CQ WPX SSB"
+carillo_name = "CQWPXSSB"
+mode = "SSB"  # CW SSB BOTH RTTY
+
+# 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
+dupe_type = 4
+
+
+def init_contest(self):
+    """setup plugin"""
+    set_tab_next(self)
+    set_tab_prev(self)
+    interface(self)
+
+
+def interface(self):
+    """Setup user interface"""
+    self.field1.show()
+    self.field2.show()
+    self.field3.show()
+    self.field4.hide()
+    label = self.field3.findChild(QtWidgets.QLabel)
+    label.setText("CQ Zone")
+
+
+def set_tab_next(self):
+    """Set TAB Advances"""
+    self.tab_next = {
+        self.callsign: self.field1.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.field2.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field2.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field3.findChild(QtWidgets.QLineEdit): self.field4.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field4.findChild(QtWidgets.QLineEdit): self.callsign,
+    }
+
+
+def set_tab_prev(self):
+    """Set TAB Advances"""
+    self.tab_prev = {
+        self.callsign: self.field4.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.callsign,
+        self.field2.findChild(QtWidgets.QLineEdit): self.field1.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field3.findChild(QtWidgets.QLineEdit): self.field2.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field4.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+    }
+
+
+def set_contact_vars(self):
+    """Contest Specific"""
+    self.contact["SNT"] = self.sent.text()
+    self.contact["RCV"] = self.receive.text()
+    self.contact["ZN"] = self.other_1.text()
+
+
+def prefill(self):
+    """xxx"""
+    field = self.field3.findChild(QtWidgets.QLineEdit)
+    if len(field.text()) == 0:
+        field.setText(str(self.contact.get("ZN", "")))
