b8a776ff6635f1c6434d5dc393405ecfd8ebd19e
Michael Bridak <michael.bridak@gmail.com>
2023-03-22 14:49:35 -0700

    changed cabrillo name

--- not1mm/plugins/cq_wpx_ssb.py
+++ not1mm/plugins/cq_wpx_ssb.py
@@ -2,7 +2,7 @@
 from PyQt5 import QtWidgets
 
 name = "CQ WPX SSB"
-carillo_name = "CQWPXSSB"
+carillo_name = "CQ-WPX-SSB"
 mode = "SSB"  # CW SSB BOTH RTTY
 
 # 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
