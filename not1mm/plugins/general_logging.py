b90a38976d5f0de232b396059d4c50dabb2f7707
Michael Bridak <michael.bridak@gmail.com>
2023-04-03 14:04:20 -0700

    add adif output.

--- not1mm/plugins/general_logging.py
+++ not1mm/plugins/general_logging.py
@@ -1,11 +1,17 @@
 """General Logging plugin"""
 
 # pylint: disable=invalid-name, unused-argument, unused-variable
+import logging
+from pathlib import Path
 
 from PyQt5 import QtWidgets
 
+from not1mm.lib.version import __version__
+
+logger = logging.getLogger("__main__")
+
 name = "General Logging"
-carillo_name = "GeneralLogging"
+cabrillo_name = "General-Logging"
 mode = "BOTH"  # CW SSB BOTH RTTY
 
 # 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
@@ -17,6 +23,7 @@ def init_contest(self):
     set_tab_next(self)
     set_tab_prev(self)
     interface(self)
+    self.next_field = self.other_1
 
 
 def interface(self):
@@ -110,6 +117,100 @@ def adif(self):
     """
     Creates an ADIF file of the contacts made.
     """
+    filename = (
+        str(Path.home())
+        + "/"
+        + f"{self.station.get('Call').upper()}_{cabrillo_name}.adi"
+    )
+    log = self.database.fetch_all_contacts_asc()
+    try:
+        with open(filename, "w", encoding="utf-8") as file_descriptor:
+            print("<ADIF_VER:5>2.2.0", end="\r\n", file=file_descriptor)
+            print("<EOH>", end="\r\n", file=file_descriptor)
+            for contact in log:
+                hiscall = contact.get("Call", "")
+                the_date_and_time = contact.get("TS")
+                # band = contact.get("Band")
+                themode = contact.get("Mode")
+                frequency = str(contact.get("Freq", 0) / 1000)
+                sentrst = contact.get("SNT", "")
+                rcvrst = contact.get("RCV", "")
+                sentnr = str(contact.get("SentNr", "59"))
+                rcvnr = str(contact.get("NR", "59"))
+                grid = contact.get("GridSquare", "")
+                comment = contact.get("ContestName", "")
+                loggeddate = the_date_and_time[:10]
+                loggedtime = the_date_and_time[11:13] + the_date_and_time[14:16]
+                print(
+                    f"<QSO_DATE:{len(''.join(loggeddate.split('-')))}:d>"
+                    f"{''.join(loggeddate.split('-'))}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+                print(
+                    f"<TIME_ON:{len(loggedtime)}>{loggedtime}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+                print(
+                    f"<CALL:{len(hiscall)}>{hiscall}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+                print(
+                    f"<MODE:{len(themode)}>{themode}", end="\r\n", file=file_descriptor
+                )
+                # print(
+                #     f"<BAND:{len(band + 'M')}>{band + 'M'}",
+                #     end="\r\n",
+                #     file=file_descriptor,
+                # )
+                try:
+                    print(
+                        f"<FREQ:{len(frequency)}>{frequency}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    pass  # This is bad form... I can't remember why this is in a try block
+
+                print(
+                    f"<RST_SENT:{len(sentrst)}>{sentrst}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+                print(
+                    f"<RST_RCVD:{len(rcvrst)}>{rcvrst}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+
+                print(
+                    f"<STX_STRING:{len(sentnr)}>{sentnr}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+                print(
+                    f"<SRX_STRING:{len(rcvnr)}>{rcvnr}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+                if len(grid) > 1:
+                    print(
+                        f"<GRIDSQUARE:{len(grid)}>{grid}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+
+                print(
+                    f"<COMMENT:{len(comment)}>{comment}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+                print("<EOR>", end="\r\n", file=file_descriptor)
+                print("", end="\r\n", file=file_descriptor)
+    except IOError:
+        ...
 
 
 def cabrillo(self):
