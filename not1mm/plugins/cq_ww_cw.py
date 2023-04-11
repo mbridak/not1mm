c990652185a45a790abecd3301bfc7b6c70fee0e
Michael Bridak <michael.bridak@gmail.com>
2023-04-11 08:40:01 -0700

    worked on cw ww

--- not1mm/plugins/cq_ww_cw.py
+++ not1mm/plugins/cq_ww_cw.py
@@ -4,8 +4,8 @@
 
 from PyQt5 import QtWidgets
 
-name = "CQ World Wide DX CW"
-carillo_name = "CQWWCW"
+name = "CQ WW CW"
+cabrillo_name = "CQ-WW-CW"
 mode = "CW"  # CW SSB BOTH RTTY
 
 # 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
