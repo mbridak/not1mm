61ec02f1bd468c5b31e0440185ebceb6eea4a6cb
Michael Bridak <michael.bridak@gmail.com>
2023-04-20 10:56:19 -0700

    Corrected logwindow column

--- not1mm/plugins/cq_wpx_cw.py
+++ not1mm/plugins/cq_wpx_cw.py
@@ -13,7 +13,7 @@ logger = logging.getLogger("__main__")
 name = "CQ WPX CW"
 cabrillo_name = "CQ-WPX-CW"
 mode = "CW"  # CW SSB BOTH RTTY
-columns = [0, 1, 2, 3, 4, 5, 6, 9, 10, 15]
+columns = [0, 1, 2, 3, 4, 5, 6, 9, 11, 15]
 
 # 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
 dupe_type = 2
