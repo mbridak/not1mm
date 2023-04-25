cc9d2225fc17f903215e0df6a50a57ac78b1b6c4
Michael Bridak <michael.bridak@gmail.com>
2023-04-25 14:17:52 -0700

    just some testing stuff

--- not1mm/test.py
+++ not1mm/test.py
@@ -5,51 +5,88 @@ Email: michael.bridak@gmail.com
 GPL V3
 """
 
-# playing with sound
-
-# pylint: disable=too-many-lines
-# pylint: disable=invalid-name
-# pylint: disable=no-member
-# pylint: disable=no-name-in-module
-# pylint: disable=c-extension-no-member
-# pylint: disable=unused-import
-
-# import threading
-
-import sounddevice as sd
-import soundfile as sf
-
-print(f"{sd.query_devices(kind='output')}")
-
-# FILENAME = "/home/mbridak/.local/share/not1mm/K6GTE/z.wav"
-# current_frame = 0
-# event = threading.Event()
-
-callsign = "k6gte 599 org"
+import sys
+from PyQt5 import QtNetwork
+from PyQt5 import QtWidgets, uic
 
 
-# def callback(outdata, frames, _time, status):
-#     """docstring"""
-#     global current_frame
-#     if status:
-#         print(status)
-#     chunksize = min(len(data) - current_frame, frames)
-#     outdata[:chunksize] = data[current_frame : current_frame + chunksize]
-#     if chunksize < frames:
-#         outdata[chunksize:] = 0
-#         raise sd.CallbackStop()
-#     current_frame += chunksize
+class MainWindow(QtWidgets.QMainWindow):
+    """
+    The main window
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi("/home/mbridak/Nextcloud/dev/not1mm/not1mm/test.ui", self)
+        bandmap_scene = QtWidgets.QGraphicsScene()
+        self.socket = QtNetwork.QTcpSocket()
+        self.socket.readyRead.connect(self.receive)
+        self.socket.connected.connect(self.connected)
+        self.socket.errorOccurred.connect(self.socket_error)
+        self.socket.connectToHost("hamqth.com", 7300)
+
+    def receive(self):
+        """doc"""
+        data = self.socket.readAll()
+        data = str(data, "utf-8").strip()
+        if "login:" in data:
+            self.send_command("k6gte")
+            self.send_command("reject/spot 0 info FT8")
+            self.send_command("accept/spot 0 freq hf/cw")
+            self.send_command("accept/spot 1 freq hf/ssb")
+        if "DX de" in data:
+            parts = data.split()
+            spotter = parts[2]
+            freq = parts[3]
+            dx = parts[4]
+            time = parts[-1]
+            print(f"{dx} {freq} {time}")
+
+    def connected(self):
+        """doc"""
+        print("Connected")
+
+    def socket_error(self):
+        """doc"""
+        print("An Error occurred.")
+
+    def disconnect(self):
+        """doc"""
+        self.socket.close()
+
+    def send_command(self, cmd: str):
+        """doc"""
+        cmd += "\r\n"
+        tosend = bytes(cmd, encoding="ascii")
+        print(f"{tosend}")
+        if self.socket:
+            if self.socket.isOpen():
+                self.socket.write(tosend)
+
+    def clearSpots(self):
+        """doc"""
+
+    def zoomIn(self):
+        """doc"""
+
+    def zoomOut(self):
+        """doc"""
+
+    def spotAgingChanged(self):
+        """doc"""
+
+    def showContextMenu(self):
+        """doc"""
 
 
-# data, fs = sf.read(FILENAME)
+def run():
+    """doc"""
+    sys.exit(app.exec())
 
 
-for letter in callsign:
-    if letter == " ":
-        letter = "space"
-    filename = f"/home/mbridak/.local/share/not1mm/K6GTE/{letter}.wav"
-    print(filename)
-    data, fs = sf.read(filename, dtype="float32")
+app = QtWidgets.QApplication(sys.argv)
 
-    sd.play(data, fs)
-    _status = sd.wait()
+window = MainWindow()
+window.show()
+if __name__ == "__main__":
+    run()
