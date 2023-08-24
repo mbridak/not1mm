f1b20017f2af246383c5c825a57cd84bb140fc08
mbridak <michael.bridak@gmail.com>
2023-08-24 10:50:27 -0700

    add update_version script

---
+++ update_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+today=$(date +'%y.%m.%d')
+today_no_leading_zeros=$(echo "$today" | sed 's/.0/./g')
+
+tnlz=$(echo version = \"$today_no_leading_zeros\")
+str=$(cat pyproject.toml | grep version)
+sed -i "s/$str/$tnlz/" pyproject.toml
+
+str=$(cat not1mm/lib/version.py | grep __version__)
+tnlz=$(echo __version__ = \"$today_no_leading_zeros\")
+sed -i "s/$str/$tnlz/" not1mm/lib/version.py
