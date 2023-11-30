3e50351540c6112a1b9f0c97280eb249386b3a4a
mbridak <michael.bridak@gmail.com>
2023-11-30 07:04:00 -0800

    Add lib for common contest plugin functions.

---
+++ not1mm/lib/plugin_common.py
@@ -0,0 +1,164 @@
+"""Common function(s) for all contest plugins"""
+
+import datetime
+from decimal import Decimal
+from pathlib import Path
+from not1mm.lib.ham_utility import get_adif_band
+
+
+def gen_adif(self, cabrillo_name: str):
+    """
+    Creates an ADIF file of the contacts made.
+    """
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+    filename = (
+        str(Path.home())
+        + "/"
+        + f"{self.station.get('Call').upper()}_{cabrillo_name}_{date_time}.adi"
+    )
+    log = self.database.fetch_all_contacts_asc()
+    try:
+        with open(filename, "w", encoding="utf-8") as file_descriptor:
+            print("<ADIF_VER:5>2.2.0", end="\r\n", file=file_descriptor)
+            print("<EOH>", end="\r\n", file=file_descriptor)
+            for contact in log:
+                hiscall = contact.get("Call", "")
+                hisname = contact.get("Name", "")
+                the_date_and_time = contact.get("TS")
+                themode = contact.get("Mode")
+                frequency = str(Decimal(str(contact.get("Freq", 0))) / 1000)
+                band = get_adif_band(Decimal(str(contact.get("Freq", 0))) / 1000)
+                # band = getband(str(int(float(frequency) * 1000000)))
+                sentrst = contact.get("SNT", "")
+                rcvrst = contact.get("RCV", "")
+                sentnr = str(contact.get("SentNr", "0"))
+                rcvnr = str(contact.get("NR", "0"))
+                grid = contact.get("GridSquare", "")
+                comment = contact.get("Comment", "")
+                loggeddate = the_date_and_time[:10]
+                loggedtime = the_date_and_time[11:13] + the_date_and_time[14:16]
+                print(
+                    f"<QSO_DATE:{len(''.join(loggeddate.split('-')))}:d>"
+                    f"{''.join(loggeddate.split('-'))}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+
+                try:
+                    print(
+                        f"<TIME_ON:{len(loggedtime)}>{loggedtime}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<CALL:{len(hiscall)}>{hiscall.upper()}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    if len(hisname):
+                        print(
+                            f"<NAME:{len(hisname)}>{hisname.title()}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<MODE:{len(themode)}>{themode}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<BAND:{len(band)}>{band}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<FREQ:{len(frequency)}>{frequency}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<RST_SENT:{len(sentrst)}>{sentrst}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    print(
+                        f"<RST_RCVD:{len(rcvrst)}>{rcvrst}",
+                        end="\r\n",
+                        file=file_descriptor,
+                    )
+                except TypeError:
+                    ...
+
+                try:
+                    if sentnr != "0":
+                        print(
+                            f"<STX_STRING:{len(sentnr)}>{sentnr}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    if rcvnr != "0":
+                        print(
+                            f"<SRX_STRING:{len(rcvnr)}>{rcvnr}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    if len(grid) > 1:
+                        print(
+                            f"<GRIDSQUARE:{len(grid)}>{grid}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                try:
+                    if len(comment):
+                        print(
+                            f"<COMMENT:{len(comment)}>{comment}",
+                            end="\r\n",
+                            file=file_descriptor,
+                        )
+                except TypeError:
+                    ...
+
+                print("<EOR>", end="\r\n", file=file_descriptor)
+                print("", end="\r\n", file=file_descriptor)
+    except IOError:
+        ...
