e0a5c14d000dd24f8d7909fba89033b2377f40e8
mbridak <michael.bridak@gmail.com>
2024-01-16 06:52:55 -0800

    add Stew Perry Topband

---
+++ not1mm/plugins/stew_perry_topband.py
@@ -0,0 +1,324 @@
+"""Stew Perry Topband plugin"""
+
+# Geographic Focus:	Worldwide
+# Participation:	Worldwide
+# Awards:	Worldwide
+# Mode:	CW
+# Bands:	160m Only
+# Classes:	Single Op (QRP/Low/High)
+# Multi-Op (QRP/Low/High)
+# Max operating hours:	14 hours
+# Max power:	HP: >100 watts
+# LP: 5-100 watts
+# QRP: <5 watts
+# Exchange:	4-Character grid square
+# QSO Points:	1 point per QSO plus 1 point per 500 km
+# multiply QSO points by 2 if low power station
+# multiply QSO points by 4 if QRP station
+# Multipliers:	(none)
+# Score Calculation:	Total score = total QSO points x power multiplier
+# Submit logs by:	January 15, 2024
+# E-mail logs to:	(none)
+# Upload log at:	http://www.b4h.net/stew/tbdcsubmitlog.php
+# Mail logs to:	BARC
+# 50335 NW Hayward Rd
+# Manning, OR 97125
+# USA
+# Find rules at:	http://www.kkn.net/stew/
+
+# pylint: disable=invalid-name, unused-argument, unused-variable, c-extension-no-member
+
+import datetime
+import logging
+
+from pathlib import Path
+from PyQt5 import QtWidgets
+from not1mm.lib.plugin_common import gen_adif, get_points
+from not1mm.lib.version import __version__
+from not1mm.lib.ham_utility import distance
+
+logger = logging.getLogger("__main__")
+
+cabrillo_name = "STEW-PERRY"
+name = "Stew Perry Topband"
+
+mode = "CW"  # CW SSB BOTH RTTY
+
+columns = [
+    "YYYY-MM-DD HH:MM:SS",
+    "Call",
+    "Freq",
+    "Mode",
+    "Exchange1",
+    "PTS",
+]
+
+advance_on_space = [True, True, True, True, True]
+
+# 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
+dupe_type = 1
+
+
+def init_contest(self):
+    """setup plugin"""
+    set_tab_next(self)
+    set_tab_prev(self)
+    interface(self)
+    self.next_field = self.other_1
+
+
+def interface(self):
+    """Setup user interface"""
+    self.field1.show()
+    self.field2.show()
+    self.field3.show()
+    self.field4.hide()
+    label = self.field3.findChild(QtWidgets.QLabel)
+    label.setText("Grid")
+    self.field3.setAccessibleName("Grid")
+
+
+def reset_label(self):
+    """reset label after field cleared"""
+
+
+def set_tab_next(self):
+    """Set TAB Advances"""
+    self.tab_next = {
+        self.callsign: self.field3.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field2.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field3.findChild(QtWidgets.QLineEdit): self.callsign,
+    }
+
+
+def set_tab_prev(self):
+    """Set TAB Advances"""
+    self.tab_prev = {
+        self.callsign: self.field3.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.callsign,
+        self.field2.findChild(QtWidgets.QLineEdit): self.callsign,
+        self.field3.findChild(QtWidgets.QLineEdit): self.callsign,
+    }
+
+
+def set_contact_vars(self):
+    """Contest Specific"""
+    self.contact["SNT"] = self.sent.text()
+    self.contact["RCV"] = self.receive.text()
+    self.contact["Exchange1"] = self.other_1.text().upper()
+
+
+def predupe(self):
+    """called after callsign entered"""
+
+
+def prefill(self):
+    """Fill SentNR"""
+
+
+def points(self):
+    """Calc point"""
+    _points = 1
+    _kilometers = 0
+    _their_grid = self.other_1.text().upper()
+    _kilometers = distance(self.station.get("GridSquare", ""), _their_grid)
+    if _kilometers > 500:
+        _points = _points + int(_kilometers / 500)
+    if self.contest_settings.get("PowerCategory", "") == "QRP":
+        _points = _points * 4
+    if self.contest_settings.get("PowerCategory", "") == "LOW":
+        _points = _points * 2
+    return _points
+
+
+def show_mults(self):
+    """Return display string for mults"""
+    return 0
+
+
+def show_qso(self):
+    """Return qso count"""
+    result = self.database.fetch_qso_count()
+    if result:
+        return int(result.get("qsos", 0))
+    return 0
+
+
+def calc_score(self):
+    """Return calculated score"""
+    _points = get_points(self)
+    return _points
+
+
+def adif(self):
+    """Call the generate ADIF function"""
+    gen_adif(self, cabrillo_name, "STEW-PERRY")
+
+
+def cabrillo(self):
+    """Generates Cabrillo file. Maybe."""
+    # https://www.cqwpx.com/cabrillo.htm
+    logger.debug("******Cabrillo*****")
+    logger.debug("Station: %s", f"{self.station}")
+    logger.debug("Contest: %s", f"{self.contest_settings}")
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+    filename = (
+        str(Path.home())
+        + "/"
+        + f"{self.station.get('Call').upper()}_{cabrillo_name}_{date_time}.log"
+    )
+    logger.debug("%s", filename)
+    log = self.database.fetch_all_contacts_asc()
+    try:
+        with open(filename, "w", encoding="ascii") as file_descriptor:
+            print("START-OF-LOG: 3.0", end="\r\n", file=file_descriptor)
+            print(
+                f"CREATED-BY: Not1MM v{__version__}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"CONTEST: {cabrillo_name}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"CALLSIGN: {self.station.get('Call','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"LOCATION: {self.station.get('ARRLSection', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            # print(
+            #     f"ARRL-SECTION: {self.pref.get('section', '')}",
+            #     end="\r\n",
+            #     file=file_descriptor,
+            # )
+            print(
+                f"CATEGORY-OPERATOR: {self.contest_settings.get('OperatorCategory','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"CATEGORY-ASSISTED: {self.contest_settings.get('AssistedCategory','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"CATEGORY-BAND: {self.contest_settings.get('BandCategory','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"CATEGORY-MODE: {self.contest_settings.get('ModeCategory','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"CATEGORY-TRANSMITTER: {self.contest_settings.get('TransmitterCategory','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            if self.contest_settings.get("OverlayCategory", "") != "N/A":
+                print(
+                    f"CATEGORY-OVERLAY: {self.contest_settings.get('OverlayCategory','')}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+            print(
+                f"GRID-LOCATOR: {self.station.get('GridSquare','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            # print(
+            #     f"CATEGORY: {None}",
+            #     end="\r\n",
+            #     file=file_descriptor,
+            # )
+            print(
+                f"CATEGORY-POWER: {self.contest_settings.get('PowerCategory','')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+
+            print(
+                f"CLAIMED-SCORE: {calc_score(self)}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                "OPERATORS: ",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"NAME: {self.station.get('Name', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"ADDRESS: {self.station.get('Street1', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"ADDRESS-CITY: {self.station.get('City', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"ADDRESS-STATE-PROVINCE: {self.station.get('State', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"ADDRESS-POSTALCODE: {self.station.get('Zip', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"ADDRESS-COUNTRY: {self.station.get('Country', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            print(
+                f"EMAIL: {self.station.get('Email', '')}",
+                end="\r\n",
+                file=file_descriptor,
+            )
+            for contact in log:
+                the_date_and_time = contact.get("TS", "")
+                themode = contact.get("Mode", "")
+                if themode == "LSB" or themode == "USB":
+                    themode = "PH"
+                frequency = str(int(contact.get("Freq", "0"))).rjust(5)
+
+                loggeddate = the_date_and_time[:10]
+                loggedtime = the_date_and_time[11:13] + the_date_and_time[14:16]
+                print(
+                    f"QSO: {frequency} {themode} {loggeddate} {loggedtime} "
+                    f"{contact.get('StationPrefix', '').ljust(13)} "
+                    f"{self.contest_settings.get('SentExchange', '').ljust(9).upper()}"
+                    f"{contact.get('Call', '').ljust(13)} "
+                    f"{str(contact.get('Exchange1', '')).ljust(3)} "
+                    f"{str(contact.get('Sect', '')).ljust(6)}",
+                    end="\r\n",
+                    file=file_descriptor,
+                )
+            print("END-OF-LOG:", end="\r\n", file=file_descriptor)
+    except IOError as exception:
+        logger.critical("cabrillo: IO error: %s, writing to %s", exception, filename)
+        return
+
+
+def recalculate_mults(self):
+    """Recalculates multipliers after change in logged qso."""
