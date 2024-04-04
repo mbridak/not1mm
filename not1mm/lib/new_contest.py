161408324b81d2efaa22f8649932c5190953441e
mbridak <michael.bridak@gmail.com>
2024-04-04 11:02:50 -0700

    Add mechanism to display an exchange hint when adding a contest.

--- not1mm/lib/new_contest.py
+++ not1mm/lib/new_contest.py
@@ -1,5 +1,6 @@
 """New Contest Dialog"""
 
+import importlib
 from PyQt6 import QtWidgets, uic
 
 
@@ -10,6 +11,16 @@ class NewContest(QtWidgets.QDialog):
         super().__init__(None)
         uic.loadUi(app_data_path / "new_contest.ui", self)
         self.buttonBox.clicked.connect(self.store)
+        self.contest.currentTextChanged.connect(self.add_exchange_hint)
 
     def store(self):
         """dialog magic"""
+
+    def add_exchange_hint(self):
+        """add hint"""
+        contest_name = self.contest.currentText().lower().replace(" ", "_")
+        temp = importlib.import_module(f"not1mm.plugins.{contest_name}")
+        if hasattr(temp, "EXCHANGE_HINT"):
+            self.exchange.setPlaceholderText(temp.EXCHANGE_HINT)
+        else:
+            self.exchange.setPlaceholderText("")
