df1f92bea4f6bf50301750b32f434558db7e0cc6
mbridak <michael.bridak@gmail.com>
2024-09-22 15:19:44 -0700

    default to prepend newline.

--- not1mm/lib/fldigi_sendstring.py
+++ not1mm/lib/fldigi_sendstring.py
@@ -7,13 +7,17 @@ class FlDigi_Comm:
     def __init__(self):
         self.target = "http://127.0.0.1:7362"
 
-    def send_string(self, message: str = None, rxafter: bool = True):
+    def send_string(
+        self, message: str = None, nlbefore: bool = True, rxafter: bool = True
+    ):
         """send string"""
         try:
             server = xmlrpc.client.ServerProxy(self.target)
             server.main.tx()
+            if nlbefore:
+                message = f"\n{message}"
             if rxafter:
-                message += "^r"
+                message = f"{message}^r"
             server.text.add_tx(message)
         except OSError:
             ...
