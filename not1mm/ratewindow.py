b36b3f586ece770b12e15d7261054407b316b6e6
mbridak <michael.bridak@gmail.com>
2025-01-23 11:33:56 -0800

    @mbridak Fixed some rate queries.

--- not1mm/ratewindow.py
+++ not1mm/ratewindow.py
@@ -48,6 +48,7 @@ class RateWindow(QDockWidget):
         self.database.current_contest = self.pref.get("contest", 0)
 
         uic.loadUi(fsutils.APP_DATA_PATH / "ratewindow.ui", self)
+        self.hide_unused()
         self.timer = QTimer()
         self.timer.timeout.connect(self.get_run_and_total_qs)
         self.timer.start(1000)
@@ -141,7 +142,7 @@ class RateWindow(QDockWidget):
         # last_hour
         # 10_last_qso
         # hundred_last_qso
-        # since_starttime
+        # since_lasthour_label since_lasthour
         # --------------------
         # time_on
         # time_off
@@ -161,47 +162,102 @@ class RateWindow(QDockWidget):
         # {'runs': 3, 'totalqs': 3}
 
         # WHERE datetime(timestamp) > datetime(current_timestamp, '-60 minutes')
-        if self.active:
-            query = f"select sum(IsRunQSO) as runs, count(*) as totalqs from dxlog where ContestNR = {self.database.current_contest};"
-            result = self.database.exec_sql(query)
+
+        if not self.active:
+            return
+
+        # Get Q's in the 60 Minutes
+        query = f"select (julianday(MAX(ts)) -  julianday(MIN(ts))) * 24 * 60 as timespan, count(*) as items from (select * from dxlog where ContestNR = {self.database.current_contest} and datetime(TS) > datetime(current_timestamp, '-60 minutes'));"
+        result = self.database.exec_sql(query)
+
+        if result.get("items", 0) < 1:
+            self.last_hour.setText("--- Q/h")
+        else:
             try:
-                sandp = result.get("totalqs", 0) - result.get("runs", 0)
-                self.run_qso.setText(f"{result.get("runs", 0)}")
-                self.sandp_qso.setText(f"{sandp}")
-                self.qso_counts.setText(f"{result.get("totalqs", 0)} pts")
-            except TypeError:
+                perhour = (60.0 / result.get("timespan", 60)) * result.get("items", 0)
+                self.last_hour.setText(str(f"{perhour:.2f} Q/h"))
+            except (ZeroDivisionError, TypeError):
                 ...
 
-            query = f"select count(*) as totalqs from dxlog where ContestNR = {self.database.current_contest} and datetime(TS) > datetime(current_timestamp, '-60 minutes');"
-            result = self.database.exec_sql(query)
-            self.last_hour.setText(f"{result.get("totalqs", 0)} Q/h")
+        # Get Q's per hour rate of the last 10 QSO's
+        query = f"SELECT (julianday(MAX(ts)) -  julianday(MIN(ts))) * 24 * 60 as timespan, count(*) as items FROM ( select * from DXLOG where ContestNR = {self.database.current_contest} ORDER by ts DESC limit 10);"
+        result = self.database.exec_sql(query)
+        if result.get("items", 0) < 10:
+            self.ten_last_qso.setText("--- Q/h")
+        else:
+            try:
+                perhour = (60.0 / result.get("timespan", 60)) * result.get("items", 0)
+                self.ten_last_qso.setText(str(f"{perhour:.2f} Q/h"))
+            except (ZeroDivisionError, TypeError):
+                ...
 
-            query = f"SELECT (julianday(MAX(ts)) -  julianday(MIN(ts))) * 24 * 60 as timespan, count(*) as items FROM DXLOG where ContestNR = {self.database.current_contest} ORDER by ts DESC limit 10;"
-            result = self.database.exec_sql(query)
-            print(f"{result=}\n{query=}")
-            # timespan items
-            if result.get("items", 0) < 2:
-                self.ten_last_qso.setText(str(f"{result.get("items", 0)} Q/h"))
-            else:
-                try:
-                    perhour = (60.0 / result.get("timespan", 60)) * result.get(
-                        "items", 0
-                    )
-                    self.ten_last_qso.setText(str(f"{perhour:.2f} Q/h"))
-                except (ZeroDivisionError, TypeError):
-                    ...
+        # Get Q's per hour rate of the last 100 QSO's
+        query = f"SELECT (julianday(MAX(ts)) -  julianday(MIN(ts))) * 24 * 60 as timespan, count(*) as items FROM (select * from DXLOG where ContestNR = {self.database.current_contest} ORDER by ts DESC limit 100);"
+        result = self.database.exec_sql(query)
+        if result.get("items", 0) < 100:
+            self.hundred_last_qso.setText("--- Q/h")
+        else:
+            try:
+                perhour = (60.0 / result.get("timespan", 60)) * result.get("items", 0)
+                self.hundred_last_qso.setText(str(f"{perhour:.2f} Q/h"))
+            except (ZeroDivisionError, TypeError):
+                ...
 
-            query = f"SELECT (julianday(MAX(ts)) -  julianday(MIN(ts))) * 24 * 60 as timespan, count(*) as items FROM DXLOG where ContestNR = {self.database.current_contest} ORDER by ts DESC limit 100;"
-            result = self.database.exec_sql(query)
-            print(f"{result=}\n{query=}")
-            # timespan items
-            if result.get("items", 0) < 2:
-                self.hundred_last_qso.setText(str(f"{result.get("items", 0)} Q/h"))
-            else:
-                try:
-                    perhour = (60.0 / result.get("timespan", 60)) * result.get(
-                        "items", 0
-                    )
-                    self.hundred_last_qso.setText(str(f"{perhour:.2f} Q/h"))
-                except (ZeroDivisionError, TypeError):
-                    ...
+        # Get rate for the current hour
+        query = f"SELECT strftime('%Y-%m-%d %H:00:00','now') as limit_stamp, strftime('%H:00:00','now') as current_hour, count(*) as items FROM DXLOG where ContestNR = {self.database.current_contest} and datetime(TS) > limit_stamp;"
+        result = self.database.exec_sql(query)
+
+        self.since_lasthour_label.setText(
+            f"Since {result.get('current_hour', '00:00:00')}z:"
+        )
+        self.since_lasthour.setText(f"{result.get('items', '0')} QSO")
+
+        # Get Run QSO's and S&P QSO's
+        query = f"select sum(IsRunQSO) as runs, count(*) as totalqs from dxlog where ContestNR = {self.database.current_contest};"
+        result = self.database.exec_sql(query)
+        try:
+            sandp = result.get("totalqs", 0) - result.get("runs", 0)
+            self.run_qso.setText(f"{result.get("runs", 0)}")
+            self.sandp_qso.setText(f"{sandp}")
+            self.qso_counts.setText(f"{result.get("totalqs", 0)} pts")
+        except TypeError:
+            ...
+
+        # Get runs for the current hour
+        query = f"SELECT strftime('%Y-%m-%d %H:00:00','now') as limit_stamp, sum(IsRunQSO) as runs, count(*) as totalqs FROM DXLOG where ContestNR = {self.database.current_contest} and datetime(TS) > limit_stamp;"
+        result = self.database.exec_sql(query)
+        try:
+            sandp = result.get("totalqs", 0) - result.get("runs", 0)
+            self.hour_run_qso.setText(f"{result.get("runs", 0)}")
+            self.hour_sandp_qso.setText(f"{sandp}")
+        except TypeError:
+            ...
+
+    def hide_unused(self):
+        self.line.hide()
+        self.label_10.hide()
+        self.time_on.hide()
+        self.label_12.hide()
+        self.time_off.hide()
+
+        self.band_mode.hide()
+        self.label_23.hide()
+        self.avg_km.hide()
+        self.label_25.hide()
+        self.avg_pts.hide()
+        self.best_dx.hide()
+        self.label_26.hide()
+
+        self.line_5.hide()
+        self.label_29.hide()
+        self.label_30.hide()
+        self.time_by_mult.hide()
+        self.label_32.hide()
+        self.qso_counts.hide()
+        self.label_34.hide()
+        self.mult_counts.hide()
+        self.label_35.hide()
+        self.mult_worth.hide()
+
+        self.line_6.hide()
+        self.label_38.hide()
