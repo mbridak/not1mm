355ac6bce7a4243dec102d5cd9e58aa597c6c4af
Aldo Mendoza <aldo.mnd@gmail.com>
2025-05-05 21:54:05 -0700

    Add settings for CW padding when sending serial number

--- not1mm/__main__.py
+++ not1mm/__main__.py
@@ -2869,7 +2869,7 @@ class MainWindow(QtWidgets.QMainWindow):
             next_serial = "1"
         macro = macro.upper()
         if self.radio_state.get("mode") == "CW":
-            macro = macro.replace("#", next_serial.rjust(3, "T"))
+            macro = macro.replace("#", next_serial.rjust(self.pref["cwpaddinglength"], self.pref["cwpaddingchar"]))
         else:
             macro = macro.replace("#", next_serial)
         macro = macro.replace("{MYCALL}", self.station.get("Call", ""))
@@ -2882,7 +2882,7 @@ class MainWindow(QtWidgets.QMainWindow):
             macro = macro.replace("{SNT}", self.sent.text())
         if self.radio_state.get("mode") == "CW":
             macro = macro.replace(
-                "{SENTNR}", self.other_1.text().lstrip("0").rjust(3, "T")
+                "{SENTNR}", self.other_1.text().lstrip("0").rjust(self.pref["cwpaddinglength"], self.pref["cwpaddingchar"])
             )
         else:
             macro = macro.replace("{SENTNR}", self.other_1.text())
