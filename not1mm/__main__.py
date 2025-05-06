8dc3af6362d90a3612d50d2bd7db729a325a33e4
JG3LLB <ja1cul@gmail.com>
2025-05-06 17:04:12 +0900

    Revert "stop_morse  Update __main__.py"
    This reverts commit d98a057fee01d3347d756f32a25f88a9227315bd.

--- not1mm/__main__.py
+++ not1mm/__main__.py
@@ -2121,10 +2121,6 @@ class MainWindow(QtWidgets.QMainWindow):
             if self.cw.servertype == 2:
                 self.cw.winkeyer_stop()
                 return
-            if self.cw.servertype == 3:
-                self.rig_control.cat.stopcwrigctl()
-                return
-
         if self.rig_control:
             if self.rig_control.online:
                 if self.pref.get("cwtype") == 3 and self.rig_control is not None:
