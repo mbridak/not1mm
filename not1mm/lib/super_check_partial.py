95186f4b69bf75e865bb587316d9ba5ec9607b87
mbridak <michael.bridak@gmail.com>
2025-06-03 06:51:08 -0700

    @mbridak Fix creash caused by SSL cert expiration from supercheckpartial.com

--- not1mm/lib/super_check_partial.py
+++ not1mm/lib/super_check_partial.py
@@ -29,14 +29,15 @@ def prefer_prefix_score(query: str, candidate: str, **kwargs) -> int:
         score = 0.8 * score
     return int(round(score))
 
+
 @lru_cache(maxsize=1024)  # You can adjust this as needed
 def prefix_bias_score(query: str, candidate: str, **kwargs) -> int:
     """Return a score based on the quality of the match."""
     score = fuzz.QRatio(query, candidate)
 
-    if candidate[:len(query)] == query:
+    if candidate[: len(query)] == query:
         return score
-    return int(score*0.8)
+    return int(score * 0.8)
 
 
 class SCP:
@@ -53,18 +54,21 @@ class SCP:
         - Returns True if successful
         - Otherwise False
         """
-        with requests.Session() as session:
-            the_request = session.get(MASTER_SCP_URL)
-            if the_request.status_code == 200:
-                with open(Path(self.app_data_path) / "MASTER.SCP", "wb+") as file:
-                    file.write(the_request.content)
-                return True
+        try:
+            with requests.Session() as session:
+                the_request = session.get(MASTER_SCP_URL)
+                if the_request.status_code == 200:
+                    with open(Path(self.app_data_path) / "MASTER.SCP", "wb+") as file:
+                        file.write(the_request.content)
+                    return True
+        except requests.exceptions.RequestException as exception:
+            logger.critical("update_masterscp: %s", exception)
         return False
 
     def read_scp(self):
         """
         Reads in a list of known contesters into an internal dictionary
-        """         
+        """
         try:
             with open(
                 Path(self.app_data_path) / "MASTER.SCP", "r", encoding="utf-8"
@@ -74,19 +78,18 @@ class SCP:
         except IOError as exception:
             logger.critical("read_scp: read error: %s", exception)
 
-
     def super_check(self, acall: str) -> list:
 
         if len(acall) > 1:
             # Compute similarity scores between `acall` and all items in `self.scp`
             similarity_scores = process.cdist(
-                [acall], self.scp, scorer=prefix_bias_score, workers=-1,score_cutoff=60
+                [acall], self.scp, scorer=prefix_bias_score, workers=-1, score_cutoff=60
             )
 
             # Sort and retrieve the top 20 matches with their indices
-            top_matches = sorted(
-                enumerate(similarity_scores[0]), key=lambda x: -x[1]
-            )[:20]
+            top_matches = sorted(enumerate(similarity_scores[0]), key=lambda x: -x[1])[
+                :20
+            ]
 
             # Extract the corresponding strings from `self.scp`
             matches = [self.scp[idx] for idx, score in top_matches if score > 0]
--- not1mm/lib/version.py
+++ not1mm/lib/version.py
@@ -1,3 +1,3 @@
 """It's the version"""
 
-__version__ = "25.6.2"
+__version__ = "25.6.3"
