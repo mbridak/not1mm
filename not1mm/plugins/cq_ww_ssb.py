e11df3a835e7bfcddce30563aa9248a7de1058ea
Robert Marshall <69984005+ki4mcw@users.noreply.github.com>
2025-08-11 18:56:40 -0400

    @ki4mcw ADIF import

--- not1mm/plugins/cq_ww_ssb.py
+++ not1mm/plugins/cq_ww_ssb.py
@@ -43,7 +43,7 @@ from pathlib import Path
 
 from PyQt6 import QtWidgets
 
-from not1mm.lib.plugin_common import gen_adif, get_points, online_score_xml
+from not1mm.lib.plugin_common import gen_adif, imp_adif, get_points, online_score_xml
 from not1mm.lib.version import __version__
 
 logger = logging.getLogger(__name__)
