146ca7a9a0e656ae94961c3726a9eb09e26dea10
Robert Marshall <69984005+ki4mcw@users.noreply.github.com>
2025-08-11 18:44:25 -0400

    @ki4mcw ADIF import

--- not1mm/plugins/10_10_spring_cw.py
+++ not1mm/plugins/10_10_spring_cw.py
@@ -7,7 +7,7 @@ import logging
 from pathlib import Path
 
 from PyQt6 import QtWidgets
-from not1mm.lib.plugin_common import gen_adif, get_points, online_score_xml
+from not1mm.lib.plugin_common import gen_adif, imp_adif, get_points, online_score_xml
 from not1mm.lib.version import __version__
 
 logger = logging.getLogger(__name__)
