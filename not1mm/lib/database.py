51b47981174e9c28f023aa3f7b7f4d6521b79cd5
Heldis <heldis.anderson77@gmail.com.com>
2025-11-08 01:16:36 +0200

    Add ES LL KV contest support: - Add ES LL KV to contest dropdown - Add dupe checking support for period-based contests - Add ES LL KV plugin with contest-specific logic

--- not1mm/lib/database.py
+++ not1mm/lib/database.py
@@ -1367,3 +1367,17 @@ class DataBase:
         except sqlite3.OperationalError as exception:
             logger.debug("%s", exception)
             return {}
+
+    def check_dupe_on_period_mode_without_band(self, call, mode, period_1, period_2) -> dict:
+        """Checks if a call is dupe on band/mode"""
+        try:
+            with sqlite3.connect(self.database) as conn:
+                conn.row_factory = self.row_factory
+                cursor = conn.cursor()
+                cursor.execute(
+                    f"select count(*) as isdupe from dxlog where Call = '{call}' and Mode = '{mode}' and ContestNR = {self.current_contest} AND TS >= '{period_1}' AND TS <= '{period_2}';"
+                )
+                return cursor.fetchone()
+        except sqlite3.OperationalError as exception:
+            logger.debug("%s", exception)
+            return {}
