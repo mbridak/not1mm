cf8b8656370f2f8c9f0bf00bb57c8a7c9668f98c
Egco <egecan2811@yandex.com>
2026-06-25 23:16:48 +0300

    implemented edi export, reverted freq changes

--- not1mm/plugins/ta_vhf_uhf_contest.py
+++ not1mm/plugins/ta_vhf_uhf_contest.py
@@ -150,6 +150,160 @@ def calc_score(self):
 def adif(self):
     gen_adif(self, cabrillo_name)
 
+def edi(self, parent=None):
+    from pathlib import Path
+    import datetime
+    from not1mm.lib.ham_utility import distance
+    import logging
+    logger = logging.getLogger(__name__)
+
+    def get_station_field(*keys, default=""):
+        for key in keys:
+            val = self.station.get(key)
+            if val:
+                return val
+        return default
+
+    log = self.database.fetch_all_contacts_asc()
+    if not log:
+        logger.info("No QSOs to export")
+        return
+
+    my_call = get_station_field("Call", "Callsign", "MyCall", default="MYCALL")
+    my_grid = get_station_field("GridSquare", "Grid", default="AA00AA")
+    operator_name = get_station_field("OperatorName", "Operator", "Name", default="Operator")
+    address = get_station_field("Address", "Adr1", default="")
+    city = get_station_field("City", "Locality", default="")
+    country = get_station_field("Country", "CountryName", default="Turkey")
+    email = get_station_field("Email", "eMail", default="")
+    contest_name = "TA 50_144_430 Contest"
+
+    timestamps = []
+    for q in log:
+        ts = q.get("Timestamp", "")
+        if ts:
+            try:
+                dt = datetime.datetime.strptime(ts, "%Y-%m-%d %H:%M:%S")
+                timestamps.append(dt)
+            except:
+                pass
+    if timestamps:
+        start_date = min(timestamps).strftime("%Y%m%d")
+        end_date = max(timestamps).strftime("%Y%m%d")
+    else:
+        start_date = datetime.datetime.now().strftime("%Y%m%d")
+        end_date = start_date
+
+    date_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = Path.home() / f"{my_call}_{cabrillo_name}_{date_time}.edi"
+
+    bands = {}
+    for q in log:
+        band = q.get("Band", "")
+        bands.setdefault(band, []).append(q)
+
+    output_lines = []
+
+    for band, band_qsos in bands.items():
+        band_qsos.sort(key=lambda x: x.get("Timestamp", ""))
+        band_display = f"{int(float(band))} MHz"
+        band_code = "6"
+
+        header = [
+            "[REG1TEST;1]",
+            f"TName={contest_name}",
+            f"TDate={start_date};{end_date}",
+            f"PCall={my_call}",
+            f"PWWLo={my_grid}",
+            "PExch=001",
+            f"PAdr1={address}",
+            "PAdr2=",
+            "PSect=SINGLE-OP",
+            f"PBand={band_display}",
+            "PClub=",
+            f"RName={operator_name}",
+            f"RCall={my_call}",
+            f"RAdr1={address}",
+            "RAdr2=",
+            "RPoCo=",
+            f"RCity={city}",
+            f"RCoun={country}",
+            "RPhon=",
+            f"RHBBS={email}",
+            "MOpe1=",
+            "MOpe2=",
+            "STXEq=",
+            "SPowe=",
+            "SRXEq=",
+            "SAnte=",
+            "SAntH=;",
+        ]
+
+        total_qsos = len(band_qsos)
+        total_points = 0
+        dx_list = []
+
+        for q in band_qsos:
+            grid = q.get("Exchange1", "")
+            if grid and len(grid) >= 4:
+                dist = distance(my_grid, grid)
+            else:
+                dist = 0
+            mult = {"50": 1, "144": 2, "432": 3}.get(str(band), 1)
+            pts = max(1, int(dist * mult))
+            total_points += pts
+            if dist > 500:
+                dx_list.append(f"{q.get('Call', '')};{grid};{int(dist)}")
+
+        summary = [
+            f"CQSOs={total_qsos};1",
+            f"CQSOP={total_points}",
+            "CWWLs=0;0;1",
+            "CWWLB=0",
+            "CExcs=0;0;1",
+            "CExcB=0",
+            f"CDXCs={len(dx_list)};0;1",
+            "CDXCB=0",
+            f"CToSc={total_points}",
+            f"CODXC={';;'.join(dx_list)}",
+            "[Remarks]",
+            "",
+            f"[QSORecords;{total_qsos}]",
+        ]
+
+        qso_lines = []
+        for q in band_qsos:
+            ts = q.get("Timestamp", "")
+            try:
+                dt = datetime.datetime.strptime(ts, "%Y-%m-%d %H:%M:%S")
+                date_str = dt.strftime("%y%m%d")
+                time_str = dt.strftime("%H%M")
+            except:
+                date_str = start_date[-6:]
+                time_str = "0000"
+
+            call = q.get("Call", "")
+            sent_rst = q.get("Sent", "59")
+            sent_nr = str(int(q.get("SentNr", "001"))).zfill(3)
+            rcv_rst = q.get("Rcv", "59")
+            rcv_nr = str(int(q.get("RcvNr", "001"))).zfill(3)
+            grid = q.get("Exchange1", "")
+            if grid and len(grid) >= 4:
+                dist = int(distance(my_grid, grid))
+            else:
+                dist = 0
+
+            line = f"{date_str};{time_str};{call};{band_code};{sent_rst};{sent_nr};{rcv_rst};{rcv_nr};;{grid};{dist};;;;"
+            qso_lines.append(line)
+
+        output_lines.extend(header)
+        output_lines.extend(summary)
+        output_lines.extend(qso_lines)
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write("\n".join(output_lines))
+
+    logger.info(f"EDI saved to {filename}")
 
 def recalculate_mults(self):
     pass
