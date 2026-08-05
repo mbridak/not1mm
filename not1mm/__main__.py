mbridak/not1mm

commit 591fb4d3a5d7453210e46fed4b851a768f7e5f7c

Create a Cluster window
The DX cluster stream was previously only visible on the console when
$SEND_CLUSTER was set. Now it's a proper widget with an input field
where the user can type commands.

The cluster login username is moved to settings. When left blank, it
defaults to the current operator.

not1mm/__main__.py
@@ -75,6 +75,7 @@ from not1mm import fsutils
 from not1mm.bandmap import BandMapWindow
 from not1mm.chat import ChatWindow
 from not1mm.checkwindow import CheckWindow
+from not1mm.clusterwindow import ClusterWindow
 from not1mm.dxcc_tracker import DXCCWindow
 from not1mm.lib.about import About
 from not1mm.lib.cwinterface import CW
@@ -172,6 +173,7 @@ class MainWindow(QtWidgets.QMainWindow):
     log_window = None
     check_window = None
     bandmap_window = None
+    cluster_window = None
     vfo_window = None
     rate_window = None
     statistics_window = None
@@ -332,6 +334,7 @@ class MainWindow(QtWidgets.QMainWindow):
         )
         self.actionLog_Window.triggered.connect(self.launch_log_window)
         self.actionBandmap.triggered.connect(self.launch_bandmap_window)
+        self.actionCluster.triggered.connect(self.launch_cluster_window)
         self.actionCheck_Window.triggered.connect(self.launch_check_window)
         self.actionRate_Window.triggered.connect(self.launch_rate_window)
         self.actionStatistics.triggered.connect(self.launch_stats_window)
@@ -757,7 +760,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.bandmap_window.hide()
         self.bandmap_window.cluster_expire.connect(self.cluster_expire_updated)
         self.bandmap_window.message.connect(self.dockwidget_message)
-        self.bandmap_window.callsignField.setText(self.pref.get("current_op", ""))
         self.bandmap_window.bandmapwindow_closed.connect(self.launch_bandmap_window)
 
         self.show_splash_msg("Setting up CheckWindow.")
@@ -857,6 +859,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.log_window.message.connect(self.dockwidget_message)
         self.log_window.logwindow_closed.connect(self.launch_log_window)
 
+        self.show_splash_msg("Setting up ClusterWindow.")
+        self.cluster_window = ClusterWindow(self.actionCluster, parent=self)
+        self.cluster_window.setObjectName("cluster-window")
+        if os.environ.get("WAYLAND_DISPLAY") and old_Qt is True:
+            self.cluster_window.setFeatures(dockfeatures)
+        self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, self.cluster_window)
+        self.cluster_window.hide()
+        self.cluster_window.message.connect(self.dockwidget_message)
+        self.cluster_window.clusterwindow_closed.connect(self.launch_cluster_window)
+
         self.clearinputs()
         self.show_splash_msg("Loading contest.")
         self.load_contest()
@@ -883,6 +895,12 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             self.bandmap_window.hide()
             self.bandmap_window.setActive(False)
+
+        self.actionCluster.setChecked(self.pref.get("clusterwindow", False))
+        if self.actionCluster.isChecked():
+            self.cluster_window.show()
+        else:
+            self.cluster_window.hide()
 
         self.actionGroup_Chat.setChecked(self.pref.get("chatwindow", False))
         if self.actionGroup_Chat.isChecked():
@@ -1372,6 +1390,9 @@ class MainWindow(QtWidgets.QMainWindow):
             if msg.get("cmd", "") == "CHECKSPOTS" and self.check_window:
                 msg["call"] = self.callsign.text()
                 self.check_window.msg_from_main(msg)
+
+            if msg.get("cmd", "") == "DX" and self.bandmap_window:
+                self.bandmap_window.msg_from_main(msg)
 
             # '{"cmd": "LOOKUP_RESPONSE", "station": "fredo", "result": {"call": "K6GTE", "aliases": "KM6HQI", "dxcc": "291", "nickname": "Mike", "fname": "Michael C", "name": "Bridak", "addr1": "2854 W Bridgeport Ave", "addr2": "Anaheim", "state": "CA", "zip": "92804", "country": "United States", "lat": "33.825460", "lon": "-117.987510", "grid": "DM13at", "county": "Orange", "ccode": "271", "fips": "06059", "land": "United States", "efdate": "2021-01-13", "expdate": "2027-11-07", "class": "G", "codes": "HVIE", "email": "michael.bridak@gmail.com", "u_views": "3049", "bio": "7232", "biodate": "2023-04-10 17:56:55", "image": "https://cdn-xml.qrz.com/e/k6gte/qsl.png", "imageinfo": "285:545:99376", "moddate": "2021-04-08 21:41:07", "MSA": "5945", "AreaCode": "714", "TimeZone": "Pacific", "GMTOffset": "-8", "DST": "Y", "eqsl": "0", "mqsl": "1", "cqzone": "3", "ituzone": "6", "born": "1967", "lotw": "1", "user": "K6GTE", "geoloc": "geocode", "name_fmt": "Michael C \\"Mike\\" Bridak"}}'
 
@@ -2411,6 +2432,15 @@ class MainWindow(QtWidgets.QMainWindow):
         else:
             self.bandmap_window.hide()
             self.bandmap_window.setActive(False)
+
+    def launch_cluster_window(self) -> None:
+        """Launch or close the cluster window"""
+        self.pref["clusterwindow"] = self.actionCluster.isChecked()
+        Preferences.save()
+        if self.actionCluster.isChecked():
+            self.cluster_window.show()
+        else:
+            self.cluster_window.hide()
 
     def launch_check_window(self) -> None:
         """Launch or close the check window"""

not1mm/bandmap.py
@@ -410,7 +410,6 @@ class BandMapWindow(QDockWidget):
     txMark = []  # noqa: RUF012
     rxMark = []  # noqa: RUF012
     rx_freq = None
-    tx_freq = None
     something = None
     lineitemlist = []  # noqa: RUF012
     textItemList = []  # noqa: RUF012
@@ -424,10 +423,6 @@ class BandMapWindow(QDockWidget):
     worked_color = QColor(128, 128, 128)
     cluster_expire = pyqtSignal(str)
     message = pyqtSignal(dict)
-    date_pattern = r"^\d{2}-[A-Za-z]{3}-\d{4}$"
-    wwv_pattern = (
-        r"(\d{2}-\w{3}-\d{4})\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(.*?)\s+<(\w+)>"
-    )
     bandmapwindow_closed = pyqtSignal()
 
     def __init__(self, action):
@@ -453,14 +448,7 @@ class BandMapWindow(QDockWidget):
         self.clearmarkedButton.setIcon(icon)
         self.zoominButton.clicked.connect(self.zoom_in)
         self.zoomoutButton.clicked.connect(self.zoom_out)
-        self.connectButton.clicked.connect(self.connect)
         self.spots = Database()
-        self.socket = QtNetwork.QTcpSocket()
-        self.test_for_data = self.socket.bytesAvailable
-        self.socket.readyRead.connect(self.receive)
-        self.socket.connected.connect(self.maybeconnected)
-        self.socket.disconnected.connect(self.disconnected)
-        self.socket.errorOccurred.connect(self.socket_error)
         self.bandmap_scene = BandMapScene(self)
         self.bandmap_scene.setFont(self.thefont)
         self.bandmap_scene.clear()
@@ -474,7 +462,6 @@ class BandMapWindow(QDockWidget):
         self.setDarkMode()
         self.update()
         self.request_workedlist()
-        self.callsignField.setText(self.settings.get("current_op", ""))
 
     def setActive(self, mode: bool):
         self.active = bool(mode)
@@ -494,7 +481,6 @@ class BandMapWindow(QDockWidget):
             try:
                 if self.rx_freq != float(packet.get("vfoa")) / 1000:
                     self.rx_freq = float(packet.get("vfoa")) / 1000
-                    self.tx_freq = self.rx_freq
                     self.center_on_rxfreq()
             except ValueError:
                 print(f"vfo value error {packet.get('vfoa')}")
@@ -532,6 +518,12 @@ class BandMapWindow(QDockWidget):
             freq = packet.get("freq", 0.0)
             spotdx = f"dx {dx} {freq}"
             self.send_command(spotdx)
+            return
+        if packet.get("cmd", "") == "DX":
+            spot = packet
+            spot["callsign"] = packet.get("dx", "")  # rename field
+            self.spots.addspot(spot, clear_freq=True)
+            self.update_stations()
             return
         if packet.get("cmd", "") == "MARKDX":
             dx = packet.get("dx", "")
@@ -604,22 +596,6 @@ class BandMapWindow(QDockWidget):
             self.text_color = QColorConstants.Black
             self.worked_color = QColor(178, 178, 178)
             self.update()
-
-    def connect(self):
-        """Connect to the cluster."""
-        if not self.callsignField.text():
-            self.callsignField.setFocus()
-            return
-        if self.connected is True:
-            self.close_cluster()
-            return
-        server = self.settings.get("cluster_server", "dxc.nc7j.com")
-        port = self.settings.get("cluster_port", 7373)
-        logger.info(f"connecting to dx cluster {server} {port}")
-        self.socket.connectToHost(server, port)
-        self.test_for_data = self.socket.bytesAvailable
-        self.connectButton.setText("Connecting")
-        self.connected = True
 
     def spot_clicked(self):
         """dunno"""
@@ -907,98 +883,6 @@ class BandMapWindow(QDockWidget):
                 self.bandmap_scene.removeItem(mark)
         currentPolygon.clear()
 
-    def receive(self) -> None:
-        """Process waiting bytes"""
-        while self.test_for_data():
-            data = self.socket.readLine()
-
-            try:
-                data = str(data, "utf-8").strip()
-            except UnicodeDecodeError:
-                continue
-
-            if os.environ.get("SEND_CLUSTER", False) is not False:  # noqa: PLW1508
-                print(f"{data}")
-
-            if (
-                "login:" in data.lower()
-                or "call:" in data.lower()
-                or "callsign:" in data.lower()
-            ):
-                self.send_command(self.callsignField.text())
-
-            if "password:" in data.lower():
-                self.send_command(self.settings.get("cluster_password", ""))
-
-            if "BEACON" in data:
-                pass
-
-            if "DX de" in data:
-                parts = data.split()
-                spotter = parts[2]
-                freq = parts[3]
-                dx = parts[4]
-                _time = parts[-1]
-                comment = " ".join(parts[5:-1])
-                spot = {}
-                spot["ts"] = datetime.now(UTC).isoformat(" ")[:19]
-                spot["callsign"] = dx
-                spot["spotter"] = spotter
-                spot["comment"] = comment
-                logger.debug(f"{spot}")
-                try:
-                    spot["freq"] = float(freq)
-                    self.spots.addspot(spot)
-                except ValueError:
-                    logger.debug(f"couldn't parse freq from datablock {data}")
-
-            if "HELLO" in data.upper():
-                self.connectButton.setText("Connected")
-                self.test_for_data = self.socket.canReadLine
-                self.send_command(self.settings.get("cluster_filter", ""))
-                self.send_command("set dx extension Section")
-                self.send_command(
-                    "set dx mode " + self.settings.get("cluster_mode", "OPEN")
-                )
-                self.send_command("sh wwv 1")
-                logger.debug(f"callsign login acknowledged {data}")
-
-            match = re.search(self.wwv_pattern, data)
-
-            if match:
-                cmd = {}
-                cmd["cmd"] = "SPACEWEATHER"
-                cmd["date"] = match.group(1)
-                cmd["hour"] = match.group(2)
-                cmd["sfi"] = match.group(3)
-                cmd["aindex"] = match.group(4)
-                cmd["kindex"] = match.group(5)
-                cmd["conditions"] = match.group(6).strip()
-                cmd["source"] = match.group(7)
-                self.message.emit(cmd)
-
-    def maybeconnected(self) -> None:
-        """Update visual state of the connect button."""
-        self.connectButton.setText("Connecting")
-
-    def socket_error(self) -> None:
-        """Oopsie"""
-        logger.warning("An Error occurred.")
-
-    def disconnected(self) -> None:
-        """Called when socket is disconnected."""
-        self.connected = False
-        self.connectButton.setText("Closed")
-
-    def send_command(self, cmd: str) -> None:
-        """Send a command to the cluster."""
-        if os.environ.get("SEND_CLUSTER", False) is not False:  # noqa: PLW1508
-            print(f">>> {cmd}")
-        tosend = bytes(cmd + "\r\n", encoding="ascii")
-        logger.debug("Command sent to the cluster")
-        if self.socket and self.socket.isOpen():
-            self.socket.write(tosend)
-
     def clear_spots(self) -> None:
         """Delete all spots from the database."""
         self.spots.delete_spots(0)
@@ -1015,17 +899,8 @@ class BandMapWindow(QDockWidget):
     def showContextMenu(self) -> None:
         """doc string for the linter"""
 
-    def close_cluster(self) -> None:
-        """Close socket connection"""
-        if self.socket and self.socket.isOpen():
-            logger.info("Closing dx cluster connection")
-            self.socket.close()
-            self.connected = False
-            self.connectButton.setText("Closed")
-
     def closeEvent(self, _event: QtGui.QCloseEvent) -> None:
         """Triggered when instance closes."""
-        self.close_cluster()
         self.action.setChecked(False)
         self.bandmapwindow_closed.emit()
         _event.accept()

not1mm/clusterwindow.py
@@ -1,0 +1,192 @@
+#!/usr/bin/env python3
+"""
+not1mm Contest logger
+Email: michael.bridak@gmail.com
+GPL V3
+Class: ClusterWindow
+Purpose: Standalone dock widget for DX cluster traffic.
+"""
+
+from datetime import datetime, timezone
+import logging
+import re
+
+from PyQt6 import uic, QtNetwork
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QDockWidget
+
+from not1mm import fsutils
+from not1mm.lib.preferences import Preferences
+
+logger = logging.getLogger(__name__)
+
+
+class ClusterWindow(QDockWidget):
+    """The Cluster window."""
+
+    message = pyqtSignal(dict)
+    clusterwindow_closed = pyqtSignal()
+
+    def __init__(self, action, parent):
+        super().__init__()
+        self.action = action
+        self.parent = parent
+
+        self.pref = Preferences.data()
+        uic.loadUi(fsutils.APP_DATA_PATH / "clusterwindow.ui", self)
+        self.setObjectName("cluster-window")
+        self.clusterOutput.document().setMaximumBlockCount(1000)
+        self.clusterInput.returnPressed.connect(self.input_to_cluster)
+        self.connectButton.clicked.connect(self.cluster_connect)
+
+        # wire up the socket
+        self.connected = False
+        self.socket = QtNetwork.QTcpSocket()
+        self.test_for_data = self.socket.bytesAvailable
+        self.socket.readyRead.connect(self.cluster_receive)
+        self.socket.connected.connect(self.cluster_maybeconnected)
+        self.socket.disconnected.connect(self.cluster_disconnected)
+        self.socket.errorOccurred.connect(self.cluster_socket_error)
+
+    def append_message(self, message: str) -> None:
+        sb = self.clusterOutput.verticalScrollBar()
+        # tolerance of 4 px to account for overscroll/elastic scrolling
+        at_bottom = sb.value() >= sb.maximum() - 4
+
+        self.clusterOutput.append(message)
+
+        if at_bottom:
+            sb.setValue(sb.maximum())
+
+    def cluster_connect(self):
+        """Connect to the cluster."""
+        if self.connected is True:
+            self.close_cluster()
+            return
+        server = self.pref.get("cluster_server", "dxc.nc7j.com")
+        port = self.pref.get("cluster_port", 7373)
+        logger.info(f"connecting to dx cluster {server} {port}")
+        self.socket.connectToHost(server, port)
+        self.test_for_data = self.socket.bytesAvailable
+        self.setWindowTitle(f"Cluster: {server}")
+        time = datetime.now(timezone.utc).strftime("%H:%M")
+        self.append_message(f"[{time}] Connecting.")
+        self.connectButton.setText("Connecting")
+        self.connected = True
+
+    def cluster_maybeconnected(self) -> None:
+        """Update visual state of the connect button."""
+        self.connectButton.setText("Connecting")
+
+    def cluster_socket_error(self) -> None:
+        """Oopsie"""
+        logger.warning(f"Socket error: {self.socket.errorString()}")
+
+    def cluster_disconnected(self) -> None:
+        """Called when socket is disconnected."""
+        self.connected = False
+        self.setWindowTitle(f"Cluster")
+        time = datetime.now(timezone.utc).strftime("%H:%M")
+        self.append_message(f"[{time}] Disconnected.")
+        self.connectButton.setText("Connect")
+
+    def close_cluster(self) -> None:
+        """Close socket connection"""
+        if self.socket and self.socket.isOpen():
+            self.socket.close()
+            self.connected = False
+
+    def cluster_send(self, cmd: str) -> None:
+        """Send a command to the cluster."""
+        self.append_message(f">>> <b>{cmd}</b>")
+        tosend = bytes(cmd + "\r\n", encoding="ascii")
+        logger.debug(f">>> {cmd}")
+        if self.socket:
+            if self.socket.isOpen():
+                self.socket.write(tosend)
+
+    def cluster_receive(self) -> None:
+        """Process waiting bytes"""
+        while self.test_for_data():
+            data = self.socket.readLine()
+
+            try:  # strip BEL chars from messages, no one wants these
+                data = str(data, "utf-8").replace("\x07", "").strip()
+            except UnicodeDecodeError:
+                continue
+
+            logger.debug(f"< {data}")
+            self.append_message(data)
+
+            if (
+                "login:" in data.lower()
+                or "call:" in data.lower()
+                or "callsign:" in data.lower()
+            ):
+                login = self.pref.get("cluster_login", "") or self.pref.get(
+                    "current_op", ""
+                )
+                self.cluster_send(login)
+
+            if "password:" in data.lower():
+                self.cluster_send(self.pref.get("cluster_password", ""))
+
+            if "HELLO" in data.upper():
+                self.test_for_data = self.socket.canReadLine
+                self.connectButton.setText("Connected")
+                self.cluster_send(self.pref.get("cluster_filter", ""))
+                self.cluster_send("set dx extension Section")
+                self.cluster_send(
+                    "set dx mode " + self.pref.get("cluster_mode", "OPEN")
+                )
+                self.cluster_send("sh wwv 1")
+                logger.debug(f"callsign login acknowledged {data}")
+
+            if "BEACON" in data:
+                pass
+
+            if "DX de" in data:
+                parts = data.split()
+                spotter = parts[2]
+                freq = parts[3]
+                dx = parts[4]
+                _time = parts[-1]
+                comment = " ".join(parts[5:-1])
+                spot = {}
+                spot["cmd"] = "DX"
+                spot["ts"] = datetime.now(timezone.utc).isoformat(" ")[:19]
+                spot["dx"] = dx
+                spot["spotter"] = spotter
+                spot["comment"] = comment
+                logger.debug(f"{spot}")
+                try:
+                    spot["freq"] = float(freq)
+                    self.message.emit(spot)
+                except ValueError:
+                    logger.debug(f"couldn't parse freq from datablock {data}")
+
+            # wwv data
+            if match := re.search(
+                r"(\d{2}-\w{3}-\d{4})\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(.*?)\s+<(\w+)>",
+                data,
+            ):
+                cmd = {}
+                cmd["cmd"] = "SPACEWEATHER"
+                cmd["date"] = match.group(1)
+                cmd["hour"] = match.group(2)
+                cmd["sfi"] = match.group(3)
+                cmd["aindex"] = match.group(4)
+                cmd["kindex"] = match.group(5)
+                cmd["conditions"] = match.group(6).strip()
+                cmd["source"] = match.group(7)
+                self.message.emit(cmd)
+
+    def input_to_cluster(self):
+        data = self.clusterInput.text()
+        self.cluster_send(data)
+        self.clusterInput.clear()
+
+    def closeEvent(self, _event: QtGui.QCloseEvent) -> None:
+        """Triggered when instance closes. Cluster connection stays open."""
+        self.action.setChecked(False)
+        _event.accept()

not1mm/data/bandmap.ui
@@ -31,57 +31,10 @@
      <number>6</number>
     </property>
     <item>
-     <layout class="QHBoxLayout" name="horizontalLayout_3">
-      <item>
-       <widget class="QLabel" name="label">
-        <property name="text">
-         <string>Callsign</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QLineEdit" name="callsignField">
-        <property name="focusPolicy">
-         <enum>Qt::FocusPolicy::ClickFocus</enum>
-        </property>
-        <property name="accessibleName">
-         <string>callsign</string>
-        </property>
-        <property name="accessibleDescription">
-         <string>callsign used to sign in to dx cluster.</string>
-        </property>
-        <property name="styleSheet">
-         <string notr="true">text-transform: uppercase;</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QPushButton" name="connectButton">
-        <property name="focusPolicy">
-         <enum>Qt::FocusPolicy::NoFocus</enum>
-        </property>
-        <property name="toolTip">
-         <string>Connect to DX cluster.</string>
-        </property>
-        <property name="accessibleName">
-         <string>connect</string>
-        </property>
-        <property name="accessibleDescription">
-         <string>connect to d x cluster.</string>
-        </property>
-        <property name="text">
-         <string>Connect</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </item>
-    <item>
      <widget class="QScrollArea" name="scrollArea">
       <property name="font">
        <font>
         <family>JetBrains Mono</family>
-        <weight>100</weight>
         <pointsize>9</pointsize>
        </font>
       </property>
@@ -97,13 +50,12 @@
          <x>0</x>
          <y>0</y>
          <width>328</width>
-         <height>579</height>
+         <height>609</height>
         </rect>
        </property>
        <property name="font">
         <font>
          <family>JetBrains Mono</family>
-         <weight>100</weight>
          <pointsize>9</pointsize>
         </font>
        </property>
@@ -125,7 +77,6 @@
           <property name="font">
            <font>
             <family>JetBrains Mono</family>
-            <weight>100</weight>
             <pointsize>6</pointsize>
            </font>
           </property>

not1mm/data/clusterwindow.ui
@@ -1,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>Cluster</class>
+ <widget class="QDockWidget" name="Cluster">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>351</width>
+    <height>231</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Cluster</string>
+  </property>
+  <widget class="QWidget" name="dockWidgetContents">
+   <layout class="QVBoxLayout" name="verticalLayout">
+    <property name="spacing">
+     <number>0</number>
+    </property>
+    <property name="leftMargin">
+     <number>0</number>
+    </property>
+    <property name="topMargin">
+     <number>0</number>
+    </property>
+    <property name="rightMargin">
+     <number>0</number>
+    </property>
+    <property name="bottomMargin">
+     <number>0</number>
+    </property>
+    <item>
+     <widget class="QTextEdit" name="clusterOutput">
+      <property name="focusPolicy">
+       <enum>Qt::FocusPolicy::NoFocus</enum>
+      </property>
+     </widget>
+    </item>
+    <item>
+     <layout class="QHBoxLayout" name="horizontalLayout">
+      <property name="topMargin">
+       <number>0</number>
+      </property>
+      <item>
+       <widget class="QLineEdit" name="clusterInput">
+        <property name="focusPolicy">
+         <enum>Qt::FocusPolicy::ClickFocus</enum>
+        </property>
+       </widget>
+      </item>
+      <item>
+       <widget class="QPushButton" name="connectButton">
+        <property name="focusPolicy">
+         <enum>Qt::FocusPolicy::NoFocus</enum>
+        </property>
+        <property name="text">
+         <string>Connect</string>
+        </property>
+       </widget>
+      </item>
+     </layout>
+    </item>
+   </layout>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>

commit 5a5f77daf17578fafbf6d4bad7a36c01d9d2c71c

Move SPOTDX to clusterwindow.py
send_command is now called cluster_send and moved to a different class.
Bug introduced by me in 591fb4d3a.

Fix #642.

not1mm/__main__.py
@@ -2714,8 +2714,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.bandmap_window.msg_from_main(cmd)
 
     def spot_dx(self) -> None:
-        """If a bandmap_window exists, send it a SPOTDX command to forward info to the cluster."""
-        if self.bandmap_window:
+        """Build a SPOTDX command and send it to the cluster window."""
+        if self.cluster_window:
             freq = self.radio_state.get("vfoa")
             dx = self.callsign.text()
             if freq and dx:
@@ -2729,7 +2729,7 @@ class MainWindow(QtWidgets.QMainWindow):
             else:
                 cmd = None
             if cmd:
-                self.bandmap_window.msg_from_main(cmd)
+                self.cluster_window.msg_from_main(cmd)
 
     def get_sn(self) -> None:
         """Generate a serial number."""

not1mm/actions.py
@@ -103,8 +103,8 @@ def SPOT_DX(self) -> None:
         cmd["cmd"] = "SPOTDX"
         cmd["dx"] = dx
         cmd["freq"] = float(int(freq) / 1000)
-        if self.bandmap_window:
-            self.bandmap_window.msg_from_main(cmd)
+        if self.cluster_window:
+            self.cluster_window.msg_from_main(cmd)
 
 
 def MARK_SPOT(self) -> None:

not1mm/bandmap.py
@@ -513,12 +513,7 @@ class BandMapWindow(QDockWidget):
                 cmd["spot"] = spot.get("callsign", "")
                 self.message.emit(cmd)
             return
-        if packet.get("cmd", "") == "SPOTDX":
-            dx = packet.get("dx", "")
-            freq = packet.get("freq", 0.0)
-            spotdx = f"dx {dx} {freq}"
-            self.send_command(spotdx)
-            return
+
         if packet.get("cmd", "") == "DX":
             spot = packet
             spot["callsign"] = packet.get("dx", "")  # rename field

not1mm/clusterwindow.py
@@ -184,6 +184,16 @@ class ClusterWindow(QDockWidget):
         self.cluster_send(data)
         self.clusterInput.clear()
 
+    def msg_from_main(self, packet):
+        """Process messages from the main screen."""
+
+        if packet.get("cmd", "") == "SPOTDX":
+            if "dx" in packet and "freq" in packet:
+                dx = packet.get("dx")
+                freq = packet.get("freq")
+                spotdx = f"dx {dx} {freq}"
+                self.cluster_send(spotdx)
+
     def closeEvent(self, _event: QtGui.QCloseEvent) -> None:
         """Triggered when instance closes. Cluster connection stays open."""
         self.action.setChecked(False)